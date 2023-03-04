f3770c5649d0b2567b71eb053728e7be203d1ff0
Roshan Srivastava <61629012+CriticAugen@users.noreply.github.com>
2023-03-04 18:00:59 +0530

    done

--- three.js_1/main.js
+++ three.js_1/main.js
@@ -77,15 +77,25 @@ tl.fromTo(".title",{opacity:0},{opacity:1})
 let mouseDown = false
 let rgb =[]
 window.addEventListener(
-  "mousedown",() => (
-    mouseDown=true
-  )
+  "mousedown",() => (mouseDown=true)
 )
 window.addEventListener(
-  "mouseup",() => (
-    mouseDown=false
-  )
+  "mouseup",() => (mouseDown=false)
 )
 window.addEventListener("mousemove",(e) => {
-
+  if(mouseDown){
+    rgb=[
+      Math.round((e.pageX/sizes.width)*255),
+      Math.round((e.pageY/sizes.height)*255),
+      150
+    ]
+    //Lets animate
+    let newColor= new THREE.Color(`rgb(${rgb.join(",")})`)
+    gsap.to(mesh.material.color,
+      {
+        r: newColor.r,
+        g: newColor.g,
+        b: newColor.b,
+      })
+  }
 })

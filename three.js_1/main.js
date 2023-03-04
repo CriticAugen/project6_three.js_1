3d4a60d6b1cfb44dedcd93171245e81c29adb947
Roshan Srivastava <61629012+CriticAugen@users.noreply.github.com>
2023-03-04 16:45:19 +0530

    added some codes

--- three.js_1/main.js
+++ three.js_1/main.js
@@ -10,6 +10,46 @@ const material = new THREE.MeshStandardMaterial({color: "#00ff83", roughness:0.1
 const mesh = new THREE.Mesh(geometry,material)
 scene.add(mesh)
 
+//Light
+const Light = new THREE.PointLight(0xffffff,1,100)
+Light.position(0,10,10)
+Light.intensity(1.25)
+scene.add(Light)
+
+//Sizes
+const sizes = {
+  width : window.innerWidth,
+  height : window.innerHeight,
+}
+
 //Camera
 const camera = new THREE.PerspectiveCamera(45, 800/600)
+camera.position.z = 20
 scene.add(camera)
+
+//Renderer
+const canvas = document.querySelector('webgl')
+const Renderer = new THREE.WebGLRenderer({canvas})
+Renderer.setSize(sizes.width,sizes.height)
+Renderer.setPixelRatio(2)
+Renderer.render(scene,camera)
+
+//Resize
+window.addEventListener('resize',()=> {
+  //update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  //update camera
+  camera.aspect = sizes.width/sizes.height
+  camera.updateProjectionMatrix()
+  Renderer.setSize(sizes.width,sizes.height)
+
+})
+
+const loop = () => {
+  controls.update()
+  Renderer.render(scene,camera)
+  window.requestAnimationFrame(loop)
+}
+
+loop()

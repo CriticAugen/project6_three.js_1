3efd00307fe5cca9ab566508afc80281863b25c3
Roshan Srivastava <61629012+CriticAugen@users.noreply.github.com>
2023-03-04 17:45:49 +0530

    added some more codes

--- three.js_1/main.js
+++ three.js_1/main.js
@@ -1,19 +1,24 @@
-import './style.css'
-import * as THREE from "three"
+import './style.css';
+import * as THREE from 'three';
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
+import gsap from 'gsap'
 
 //Scene
 const scene = new THREE.Scene()
 
 //Create Sphere
 const geometry = new THREE.SphereGeometry(3,64,64)
-const material = new THREE.MeshStandardMaterial({color: "#00ff83", roughness:0.15})
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83", 
+  roughness:0.25,
+})
 const mesh = new THREE.Mesh(geometry,material)
 scene.add(mesh)
 
 //Light
 const Light = new THREE.PointLight(0xffffff,1,100)
-Light.position(0,10,10)
-Light.intensity(1.25)
+Light.position.set(0,10,10)
+// Light.intensity(1.25)
 scene.add(Light)
 
 //Sizes
@@ -23,16 +28,24 @@ const sizes = {
 }
 
 //Camera
-const camera = new THREE.PerspectiveCamera(45, 800/600)
+const camera = new THREE.PerspectiveCamera(45, 800 / 600)
 camera.position.z = 20
 scene.add(camera)
 
 //Renderer
-const canvas = document.querySelector('webgl')
-const Renderer = new THREE.WebGLRenderer({canvas})
+const canvas = document.querySelector('.webgl');
+const Renderer = new THREE.WebGLRenderer({canvas});
 Renderer.setSize(sizes.width,sizes.height)
 Renderer.setPixelRatio(2)
 Renderer.render(scene,camera)
+
+//Controls
+const controls = new OrbitControls(camera,canvas)
+controls.enableDamping = true
+controls.enablePan = false
+controls.enableZoom = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 5
 
 //Resize
 window.addEventListener('resize',()=> {
@@ -53,3 +66,26 @@ const loop = () => {
 }
 
 loop()
+
+//Timeline magic
+const tl = gsap.timeline({defaults:{duration:1}})
+
+tl.fromTo(mesh.scale,{z:0,x:0,y:0},{z:1,x:1,y:1})
+tl.fromTo(".title",{opacity:0},{opacity:1})
+
+//Mouse Animation Color
+let mouseDown = false
+let rgb =[]
+window.addEventListener(
+  "mousedown",() => (
+    mouseDown=true
+  )
+)
+window.addEventListener(
+  "mouseup",() => (
+    mouseDown=false
+  )
+)
+window.addEventListener("mousemove",(e) => {
+
+})
